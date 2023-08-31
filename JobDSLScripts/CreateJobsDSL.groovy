2e5ec1869f4971d72dd77135f31e4504dee22142
SONU LODHA <57846183+sonulodha@users.noreply.github.com>
2023-08-31 23:57:17 +0530

    Update CreateJobsDSL.groovy

--- JobDSLScripts/CreateJobsDSL.groovy
+++ JobDSLScripts/CreateJobsDSL.groovy
@@ -1,18 +1,23 @@
-// JobDSLScripts/CreateJobsDSL.groovy
-
 def environments = ['dev', 'qa']
 
+// Loop through each environment
 environments.each { env ->
+    // Create a folder for each environment
     folder("pulumi/${env}") {
         displayName("Pulumi - ${env}")
         
+        // Create a pipeline job for each environment
         job("Pipeline_${env}") {
             displayName("Pipeline - ${env}")
             description("Pipeline for ${env} environment")
             
+            // Define the pipeline steps
             steps {
                 script {
+                    // Read Jenkinsfile content from workspace
                     def jenkinsfileContent = readFileFromWorkspace("Jenkinsfiles/Jenkinsfile_${env}")
+                    
+                    // Execute Job DSL script to create pipeline job
                     jobDsl {
                         sandbox()
                         script(jenkinsfileContent)
