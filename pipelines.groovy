3068ec1c535734debc52763fdebceb4bcdf1cde0
SONU LODHA <57846183+sonulodha@users.noreply.github.com>
2023-10-20 03:27:54 +0530

    Update pipelines.groovy

--- pipelines.groovy
+++ pipelines.groovy
@@ -9,7 +9,7 @@ services.each { service ->
 def loadServices() {
     // Replace with the raw URL of services.json from your GitHub repository.
     // Make sure to use the raw content URL.
-    def url = "https://github.com/sonulodha/jobDsl/blob/master/aud-dev-1/services.json"
+    def url = "https://raw.githubusercontent.com/sonulodha/jobDsl/master/aud-dev-1/services.json"
     
     // Fetching the content of services.json using curl.
     def fileContent = sh(script: "curl -s '${url}'", returnStdout: true).trim()
