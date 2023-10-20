ad8a0abd90adbe9c03a1501686ecce0f119a0d30
SONU LODHA <57846183+sonulodha@users.noreply.github.com>
2023-10-20 17:31:49 +0530

    Update pipelineScript.groovy

--- pipelineScript.groovy
+++ pipelineScript.groovy
@@ -1,1 +1,21 @@
 
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                echo "Building the ${service.name} project..."
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "Running tests ${service.service_repo}..."
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "Deploying using ArgoCD file: ${service.argocdFile}"
+            }
+        }
+    }
+}
